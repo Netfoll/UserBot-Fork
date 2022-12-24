b649a2e5cfa6c0d132f806dbe1dd7d54e8230bbd
Morri <igromangtx@Mail.ru>
2022-12-24 19:22:51 +0300

    netfoll name fix

--- hikka/modules/updater.py
+++ hikka/modules/updater.py
@@ -229,7 +229,7 @@ class UpdaterMod(loader.Module):
                 if self._client.hikka_me.premium
                 and CUSTOM_EMOJIS
                 and isinstance(msg_obj, Message)
-                else "Hikka"
+                else "Netfoll"
             )
             if "LAVHOST" not in os.environ
             else self.strings("lavhost_restart").format(
