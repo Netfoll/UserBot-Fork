7d193e368e8b7a9bab51df1df7a1dd08a8e2cd6b
Morri <igromangtx@Mail.ru>
2023-01-31 14:07:26 +0300

    Update netinfo.py

--- hikka/modules/netinfo.py
+++ hikka/modules/netinfo.py
@@ -174,8 +174,6 @@ class HikkaInfoMod(loader.Module):
                 platform=platform,
                 upd=upd,
                 uptime=utils.formatted_uptime(),
-                cpu_usage=utils.get_cpu_usage(),
-                ram_usage=f"{utils.get_ram_usage()} MB",
                 branch=version.branch,
             )
             if self.config["custom_message"]
