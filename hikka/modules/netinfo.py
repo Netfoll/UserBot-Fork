82f85c2703082d8e3b187de39a138668ad5fe46a
ToXic2290 <derzkiy8233@gmail.com>
2023-02-01 21:57:08 +0600

    Update netinfo.py

--- hikka/modules/netinfo.py
+++ hikka/modules/netinfo.py
@@ -237,7 +237,7 @@ class NetfollInfoMod(loader.Module):
 
     @loader.command(ru_doc="Отправить информацию по типу 'Что такое Netfoll?'",)
     async def whonetfoll(self, message: Message):
-        """Send info aka 'What is Hikka?'"""
+        """Send info aka 'What is Netfoll?'"""
         await utils.answer(message, self.strings("desc"))
 
     @loader.command(ru_doc="<текст> - Изменить текст в .info",)
