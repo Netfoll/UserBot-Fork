50b618a1e356a3151da4446ee03a0981c836d9c8
Morri <igromangtx@Mail.ru>
2023-02-03 00:33:48 +0300

    fix prefix in help

--- hikka/modules/help.py
+++ hikka/modules/help.py
@@ -39,7 +39,7 @@ class HelpMod(loader.Module):
             "<emoji document_id=5312383351217201533>☝️</emoji> <b>This is a core"
             " module. You can't unload it nor replace</b>"
         ),
-        "info": "<emoji document_id=6334760737906362392>⚡️</emoji><b> You didn't specify a module to search for</b>\n\n<i>The installed modules can be viewed in</i> <code>.mods</code>"
+        "info": "<emoji document_id=6334760737906362392>⚡️</emoji><b> You didn't specify a module to search for</b>\n\n<i>The installed modules can be viewed in</i> <code>{}mods</code>"
     }
 
     strings_ru = {
@@ -57,7 +57,7 @@ class HelpMod(loader.Module):
             "<emoji document_id=6328010818843575869>ℹ️</emoji> <b>Это встроенный"
             " модуль. Вы не можете его выгрузить или заменить</b>"
         ),
-        "info": "<emoji document_id=6334760737906362392>⚡️</emoji><b> Вы не указали модуль для поиска</b>\n\n<i>Установленные модули можно посмотреть в</i> <code>.mods</code>"
+        "info": "<emoji document_id=6334760737906362392>⚡️</emoji><b> Вы не указали модуль для поиска</b>\n\n<i>Установленные модули можно посмотреть в</i> <code>{}mods</code>"
     }
 
     def find_aliases(self, command: str) -> list:
@@ -209,16 +209,20 @@ class HelpMod(loader.Module):
     async def help(self, message: Message):
         """[module] [-f] - Show help"""
         args = utils.get_args_raw(message)
+        prefix = f"{self.strings('info').format(str(self.get_prefix()))}\n"
         if "-f" in args:
             args = args.replace(" -f", "").replace("-f", "")
 
         if args:
             await self.modhelp(message, args)
             return
+        prefix += (
+            f"\n\n {prefix}"
+        )     
         await utils.answer(
             message,
-            self.info(message),
-            ),
+            prefix,
+            ),  
 
     @loader.command(
         ru_doc="Показать ссылку на чат помощи Hikka",
