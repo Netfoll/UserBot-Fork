7146c761bcb0147f93e7322fe2a654131b283751
Morri <igromangtx@Mail.ru>
2023-02-05 01:23:23 +0300

    Update hikka_backup.py

--- hikka/modules/hikka_backup.py
+++ hikka/modules/hikka_backup.py
@@ -28,7 +28,7 @@ class HikkaBackupMod(loader.Module):
     strings = {
         "name": "HikkaBackup",
         "period": (
-            "⌚️ <b>Unit «Netfoll-ALPHA»</b> creates database backups periodically. You can"
+            "⌚️ <b>Unit «ALPHA»</b> creates database backups periodically. You can"
             " change this behavior later.\n\nPlease, select the periodicity of"
             " automatic database backups"
         ),
