b422f249ad7d233c1b00d6ebd4ebdca59b617436
ToXic2290 <derzkiy8233@gmail.com>
2023-02-16 18:28:10 +0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -35,14 +35,17 @@ clear
 clear
 
 printf "\n\e[1;35;47m                   \e[0m"
-printf "\n\e[1;35;47m █ █ █ █▄▀ █▄▀ ▄▀█ \e[0m"
-printf "\n\e[1;35;47m █▀█ █ █ █ █ █ █▀█ \e[0m"
-printf "\n\e[1;35;47m                   \e[0m"
-printf "\n\n\e[3;34;40m Установка. Пожалуйста, подождите...\e[0m\n\n"
+printf "\n\e[1;35;47m  _   _      _    __       _ _  \e[0m"
+printf "\n\e[1;35;47m | \ | | ___| |_ / _| ___ | | | \e[0m"
+printf "\n\e[1;35;47m |  \| |/ _ \ __| |_ / _ \| | | \e[0m"
+printf "\n\e[1;35;47m | |\  |  __/ |_|  _| (_) | | | \e[0m"
+printf "\n\e[1;35;47m |_| \_|\___|\__|_|  \___/|_|_| \e[0m"
+printf "\n\e[1;35;47m                                \e[0m"
+printf "\n\e[3;34;40m Установка. Пожалуйста, подождите...\e[0m\n\n"
 
 ##############################################################################
 
-printf "\r\033[0;34mPreparing for installation...\e[0m"
+printf "\r\033[0;34mПопытка установки...\e[0m"
 
 touch netfoll-install.log
 if [ ! x"$SUDO_USER" = x"" ]; then
@@ -51,7 +54,7 @@ fi
 
 if [ -d "Netfoll/netfoll" ]; then
 	cd netfoll || {
-		printf "\rError: Install git package and re-run installer"
+		printf "\rОшибка: установите гит прежде чем продолжить (pkg/apt install git)"
 		exit 6
 	}
 	DIR_CHANGED="yes"
@@ -72,7 +75,7 @@ fi
 
 ##############################################################################
 
-echo "Installing..." >netfoll-install.log
+echo "Установка..." >netfoll-install.log
 
 if echo "$OSTYPE" | grep -qE '^linux-gnu.*' && [ -f '/etc/debian_version' ]; then
 	PKGMGR="apt install -y"
@@ -93,20 +96,20 @@ elif echo "$OSTYPE" | grep -qE '^darwin.*'; then
 	PKGMGR="brew install"
 	PYVER="3"
 else
-	printf "\r\033[1;31mUnrecognised OS.\e[0m Please follow 'Manual installation' at \033[0;94mhttps://github.com/hikariatama/Hikka/#-installation\e[0m"
+	printf "\r\033[1;31mНеизвестная OS.\e[0m Пожалуйста прочтите 'Manual installation' at \033[0;94mhttps://github.com/hikariatama/Hikka/#-installation\e[0m"
 	exit 1
 fi
 
 ##############################################################################
 
 runout "$SUDO_CMD $PKGMGR python$PYVER" git || {
-	errorout "Core install failed."
+	errorout "Ошибка установки ядра."
 	exit 2
 }
 
 
-printf "\r\033[K\033[0;32mPreparation complete!\e[0m"
-printf "\n\r\033[0;34mInstalling linux packages...\e[0m"
+printf "\r\033[K\033[0;32mПодготовка выполнена!\e[0m"
+printf "\n\r\033[0;34mУстановка дополнений linux...\e[0m"
 
 if echo "$OSTYPE" | grep -qE '^linux-gnu.*'; then
 	runout "$SUDO_CMD $PKGMGR python$PYVER-dev"
@@ -132,29 +135,29 @@ printf "\n\r\033[0;34mCloning repo...\e[0m"
 ${SUDO_CMD}rm -rf Netfoll
 # shellcheck disable=SC2086
 runout ${SUDO_CMD}git clone https://github.com/MXRRI/Netfoll || {
-	errorout "Clone failed."
+	errorout "Ошибка клонирования."
 	exit 3
 }
 cd Netfoll || {
-	printf "\r\033[0;33mRun: \033[1;33mpkg install git\033[0;33m and restart installer"
+	printf "\r\033[0;33mВведи: \033[1;33mpkg install git\033[0;33m и повтори установку."
 	exit 7
 }
 
-printf "\r\033[K\033[0;32mRepo cloned!\e[0m"
-printf "\n\r\033[0;34mInstalling python dependencies...\e[0m"
+printf "\r\033[K\033[0;32mРепо клонирован!\e[0m"
+printf "\n\r\033[0;34mУстановка необходимых компонентов...\e[0m"
 
 # shellcheck disable=SC2086
 runin "$SUDO_CMD python$PYVER" -m pip install --upgrade pip setuptools wheel --user
 # shellcheck disable=SC2086
 runin "$SUDO_CMD python$PYVER" -m pip install -r requirements.txt --upgrade --user --no-warn-script-location --disable-pip-version-check || {
-	errorin "Requirements failed!"
+	errorin "Ошибка зависимостей!"
 	exit 4
 }
 rm -f ../netfoll-install.log
 touch .setup_complete
 
 printf "\r\033[K\033[0;32mDependencies installed!\e[0m"
-printf "\n\033[0;32mStarting...\e[0m\n\n"
+printf "\n\033[0;32mДобро пожаловать...\e[0m\n\n"
 
 ${SUDO_CMD}"python$PYVER" -m netfoll "$@" || {
 	printf "\033[1;31mPython scripts failed\e[0m"
