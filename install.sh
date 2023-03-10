e22447b50f6ce5b7bb2ce51a2e6b42d3446513a6
ToXic2290 <derzkiy8233@gmail.com>
2023-03-10 21:41:49 +0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,23 +3,23 @@
 
 runin() {
 	# Runs the arguments, piping stderr to logfile
-	{ "$@" 2>>../hikka-install.log || return $?; } | while read -r line; do
-		printf "%s\n" "$line" >>../hikka-install.log
+	{ "$@" 2>>../netfoll-install.log || return $?; } | while read -r line; do
+		printf "%s\n" "$line" >>../netfoll-install.log
 	done
 }
 
 runout() {
 	# Runs the arguments, piping stderr to logfile
-	{ "$@" 2>>hikka-install.log || return $?; } | while read -r line; do
-		printf "%s\n" "$line" >>hikka-install.log
+	{ "$@" 2>>netfoll-install.log || return $?; } | while read -r line; do
+		printf "%s\n" "$line" >>netfoll-install.log
 	done
 }
 
 errorin() {
-	cat ../hikka-install.log
+	cat ../netfoll-install.log
 }
 errorout() {
-	cat hikka-install.log
+	cat netfoll-install.log
 }
 
 SUDO_CMD=""
@@ -35,23 +35,26 @@ clear
 clear
 
 printf "\n\e[1;35;47m                   \e[0m"
-printf "\n\e[1;35;47m █ █ █ █▄▀ █▄▀ ▄▀█ \e[0m"
-printf "\n\e[1;35;47m █▀█ █ █ █ █ █ █▀█ \e[0m"
-printf "\n\e[1;35;47m                   \e[0m"
-printf "\n\n\e[3;34;40m Установка. Пожалуйста, подождите...\e[0m\n\n"
+printf "\n\e[1;35;47m  _   _      _    __       _ _  \e[0m"
+printf "\n\e[1;35;47m | \ | | ___| |_ / _| ___ | | | \e[0m"
+printf "\n\e[1;35;47m |  \| |/ _ \ __| |_ / _ \| | | \e[0m"
+printf "\n\e[1;35;47m | |\  |  __/ |_|  _| (_) | | | \e[0m"
+printf "\n\e[1;35;47m |_| \_|\___|\__|_|  \___/|_|_| \e[0m"
+printf "\n\e[1;35;47m                                \e[0m"
+printf "\n\e[3;34;40m Установка. Пожалуйста, подождите...\e[0m\n\n"
 
 ##############################################################################
 
-printf "\r\033[0;34mPreparing for installation...\e[0m"
+printf "\r\033[0;34mПопытка установки...\e[0m"
 
-touch hikka-install.log
+touch netfoll-install.log
 if [ ! x"$SUDO_USER" = x"" ]; then
-	chown "$SUDO_USER:" hikka-install.log
+	chown "$SUDO_USER:" netfoll-install.log
 fi
 
-if [ -d "Netfoll/hikka" ]; then
-	cd Hikka || {
-		printf "\rError: Install git package and re-run installer"
+if [ -d "Netfoll/netfoll" ]; then
+	cd netfoll || {
+		printf "\rОшибка: установите гит прежде чем продолжить (pkg/apt install git)"
 		exit 6
 	}
 	DIR_CHANGED="yes"
@@ -64,7 +67,7 @@ if [ -f ".setup_complete" ]; then
 	fi
 	printf "\rExisting installation detected"
 	clear
-	"python$PYVER" -m hikka "$@"
+	"python$PYVER" -m netfoll "$@"
 	exit $?
 elif [ "$DIR_CHANGED" = "yes" ]; then
 	cd ..
@@ -72,7 +75,7 @@ fi
 
 ##############################################################################
 
-echo "Installing..." >hikka-install.log
+echo "Установка..." >netfoll-install.log
 
 if echo "$OSTYPE" | grep -qE '^linux-gnu.*' && [ -f '/etc/debian_version' ]; then
 	PKGMGR="apt install -y"
@@ -93,20 +96,20 @@ elif echo "$OSTYPE" | grep -qE '^darwin.*'; then
 	PKGMGR="brew install"
 	PYVER="3"
 else
-	printf "\r\033[1;31mUnrecognised OS.\e[0m Please follow 'Manual installation' at \033[0;94mhttps://github.com/hikariatama/Hikka/#-installation\e[0m"
+	printf "\r\033[1;31mНеизвестная OS.\e[0m Пожалуйста прочтите 'Manual installation' at \033[0;94mhttps://github.com/hikariatama/Hikka/#-installation\e[0m"
 	exit 1
 fi
 
 ##############################################################################
 
 runout "$SUDO_CMD $PKGMGR python$PYVER" git || {
-	errorout "Core install failed."
+	errorout "Ошибка установки ядра."
 	exit 2
 }
 
 
-printf "\r\033[K\033[0;32mPreparation complete!\e[0m"
-printf "\n\r\033[0;34mInstalling linux packages...\e[0m"
+printf "\r\033[K\033[0;32mПодготовка выполнена!\e[0m"
+printf "\n\r\033[0;34mУстановка дополнений linux...\e[0m"
 
 if echo "$OSTYPE" | grep -qE '^linux-gnu.*'; then
 	runout "$SUDO_CMD $PKGMGR python$PYVER-dev"
@@ -132,31 +135,31 @@ printf "\n\r\033[0;34mCloning repo...\e[0m"
 ${SUDO_CMD}rm -rf Netfoll
 # shellcheck disable=SC2086
 runout ${SUDO_CMD}git clone https://github.com/MXRRI/Netfoll || {
-	errorout "Clone failed."
+	errorout "Ошибка клонирования."
 	exit 3
 }
 cd Netfoll || {
-	printf "\r\033[0;33mRun: \033[1;33mpkg install git\033[0;33m and restart installer"
+	printf "\r\033[0;33mВведи: \033[1;33mpkg install git\033[0;33m и повтори установку."
 	exit 7
 }
 
-printf "\r\033[K\033[0;32mRepo cloned!\e[0m"
-printf "\n\r\033[0;34mInstalling python dependencies...\e[0m"
+printf "\r\033[K\033[0;32mРепо клонирован!\e[0m"
+printf "\n\r\033[0;34mУстановка необходимых компонентов...\e[0m"
 
 # shellcheck disable=SC2086
 runin "$SUDO_CMD python$PYVER" -m pip install --upgrade pip setuptools wheel --user
 # shellcheck disable=SC2086
 runin "$SUDO_CMD python$PYVER" -m pip install -r requirements.txt --upgrade --user --no-warn-script-location --disable-pip-version-check || {
-	errorin "Requirements failed!"
+	errorin "Ошибка зависимостей!"
 	exit 4
 }
-rm -f ../hikka-install.log
+rm -f ../netfoll-install.log
 touch .setup_complete
 
 printf "\r\033[K\033[0;32mDependencies installed!\e[0m"
-printf "\n\033[0;32mStarting...\e[0m\n\n"
+printf "\n\033[0;32mДобро пожаловать...\e[0m\n\n"
 
-${SUDO_CMD}"python$PYVER" -m hikka "$@" || {
+${SUDO_CMD}"python$PYVER" -m netfoll "$@" || {
 	printf "\033[1;31mPython scripts failed\e[0m"
 	exit 5
 }
